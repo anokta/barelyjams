abbedbb3a3efa9b5e5343fdf2fc0c1f4b855037b
alper gungormusler <anokta@gmail.com>
2024-12-25 22:14:48 -0500

    add interactable arrow

--- 02_tile_tunes/Assets/TileTunes/Scripts/TileCharacterController.cs
+++ 02_tile_tunes/Assets/TileTunes/Scripts/TileCharacterController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Barely;
 using Barely.Examples;
 using UnityEngine;
@@ -13,45 +14,70 @@ public class TileCharacterController : MonoBehaviour {
   public Instrument instrument;
   public Metronome metronome;
 
+  public Scale scale;
+
   private readonly float POSITION_LERP_SPEED = 8.0f;
   private Vector3 _targetPosition = Vector3.zero;
 
+  private Vector3 _initPosition = Vector3.zero;
+
   private bool _hasPressed = false;
 
-  public void OnBeat(int bar, int beat) {
-    var position = tilemap.WorldToCell(_targetPosition);
-    // Debug.Log("Current position = " + position);
+  private Dictionary<Vector3Int, int> _scaleDegrees = null;
+  private int _currentScaleDegree = 0;
 
-    var tileTransform = tilemap.GetTransformMatrix(position);
-    // TODO: only flip interactable arrows?
+  public void OnBeat(int bar, int beat) {
+    // TODO: temp reset
+    if (bar == 0 && beat == 0) {
+      _targetPosition = _initPosition;
+    }
+
+    var cell = tilemap.WorldToCell(_targetPosition);
+    // Debug.Log("Current cell = " + cell);
+
+    // Interact with the tile.
+    var tileTransform = tilemap.GetTransformMatrix(cell);
     if (_hasPressed) {
       _hasPressed = false;
-      tileTransform *= Matrix4x4.Rotate(Quaternion.Euler(0.0f, 0.0f, 180.0f));
-      tilemap.SetTransformMatrix(position, tileTransform);
+      if (IsInteractable(cell)) {
+        tileTransform *= Matrix4x4.Rotate(Quaternion.Euler(0.0f, 0.0f, 180.0f));
+        tilemap.SetTransformMatrix(cell, tileTransform);
+      }
     }
+
+    // Play note.
+    int scaleDegree = 0;
+    if (_scaleDegrees.TryGetValue(cell, out scaleDegree)) {
+      _currentScaleDegree = scaleDegree + 1;
+    } else {
+      scaleDegree = _currentScaleDegree++;
+      _scaleDegrees.Add(cell, scaleDegree);
+    }
+    float pitch = scale.GetPitch(scaleDegree);
+    instrument.SetNoteOn(pitch);
+    instrument.SetNoteOff(pitch);
+
+    characterSprite.color = Color.white;
 
     // TODO: assuming direction tile
     _targetPosition += tileTransform.rotation * Vector3.right;
-
-    var tile = tilemap.GetTile(position);
-    // Debug.Log("Current tile = " + tile);
-
-    instrument.SetNoteOn(0.0f);
-    instrument.SetNoteOff(0.0f);
-
-    characterSprite.color = Color.white;
   }
 
   private void OnEnable() {
+    _initPosition = character.position;
     _targetPosition = character.position;
+    _scaleDegrees = new Dictionary<Vector3Int, int>();
   }
 
   private void Update() {
     // TODO: Change input method and the position threshold.
     if (Input.GetKeyDown(KeyCode.S)) {
       if (Math.Abs(Math.Round(metronome.Position) - metronome.Position) < 0.5) {
-        instrument.SetNoteOn(1.0f);
-        instrument.SetNoteOff(1.0f);
+        float pitch = scale.GetPitch(IsInteractable(tilemap.WorldToCell(_targetPosition))
+                                         ? scale.PitchCount
+                                         : -scale.PitchCount);
+        instrument.SetNoteOn(pitch);
+        instrument.SetNoteOff(pitch);
         _hasPressed = true;
       }
     }
@@ -61,5 +87,9 @@ public class TileCharacterController : MonoBehaviour {
 
     characterSprite.color =
         Color.Lerp(characterSprite.color, new Color(0.0f, 0.0f, 0.0f, 0.0f), Time.deltaTime);
+  }
+
+  private bool IsInteractable(Vector3Int cell) {
+    return tilemap.GetTile(cell).name.Contains("interact");
   }
 }
