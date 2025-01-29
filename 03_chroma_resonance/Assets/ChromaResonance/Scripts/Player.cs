1062866688ab05bde3cdd88815011171ac26c472
alper gungormusler <anokta@gmail.com>
2025-01-28 22:56:52 -0500

    add clicker

--- 03_chroma_resonance/Assets/ChromaResonance/Scripts/Player.cs
+++ 03_chroma_resonance/Assets/ChromaResonance/Scripts/Player.cs
@@ -10,10 +10,40 @@ public class Player : MonoBehaviour {
 
   private double _lastPosition = 0.0;
 
+  public float clickScale = 0.8f;
+  public float clickScaleSpeed = 1.0f;
+  // public Color clickColor = Color.white;
+  // private Renderer[] _renderers;
+  // private Color[] _rendererColors;
+
+  void Start() {
+    // _renderers = fork.gameObject.GetComponentsInChildren<Renderer>();
+    // _rendererColors = new Color[_renderers.Length];
+    // for (int i = 0; i < _rendererColors.Length; ++i) {
+    //   _rendererColors[i] = _renderers[i].material.color;
+    // }
+  }
+
   void Update() {
+    if (Input.GetMouseButtonDown(0)) {
+      OnClick();
+    }
+
     double duration = performer.Position - _lastPosition;
     _lastPosition = performer.Position;
 
     fork.Rotate((float)duration * forkRotationSpeed * Vector3.one, Space.Self);
+    fork.localScale = Vector3.Lerp(fork.localScale, Vector3.one, Time.deltaTime * clickScaleSpeed);
+    // for (int i = 0; i < _rendererColors.Length; ++i) {
+    //   _renderers[i].material.color =
+    //       Color.Lerp(_renderers[i].material.color, _rendererColors[i], Time.deltaTime);
+    // }
+  }
+
+  void OnClick() {
+    fork.localScale = clickScale * Vector3.one;
+    // for (int i = 0; i < _rendererColors.Length; ++i) {
+    //   _renderers[i].material.color = clickColor;
+    // }
   }
 }
