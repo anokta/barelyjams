c60b68551f0af646cc5ddb67ecf859002d580728
alper gungormusler <anokta@gmail.com>
2025-02-02 21:13:55 -0500

    generalize mouse interaction

--- 03_chroma_resonance/Assets/ChromaResonance/Scripts/Player.cs
+++ 03_chroma_resonance/Assets/ChromaResonance/Scripts/Player.cs
@@ -3,51 +3,71 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour {
+  public CharacterController character;
   public Transform fork;
 
   public Repeater repeater;
 
   public FollowerAutomaton automaton;
 
-  public float idleScale = 0.5f;
-  public float clickScale = 0.4f;
+  public float clickScale = 0.8f;
+  public float hoverScale = 1.2f;
   public float clickScaleSpeed = 1.0f;
+  private Vector3 _idleScale = Vector3.one;
 
-  private float _playerOriginY = 0.0f;
-  private float _referenceFrequency = 0.0f;
+  public float maxInteractionDistance = 5.0f;
 
-  void Start() {
-    _playerOriginY = transform.position.y;
-    _referenceFrequency = Musician.ReferenceFrequency;
+  private Instrument _instrument = null;
+
+  void Awake() {
+    _idleScale = fork.localScale;
+    _instrument = GetComponent<Instrument>();
   }
 
+  void Start() {
+    _instrument.SetNoteOn(0.0f);
+  }
+
+  // private float _playerOriginY = 0.0f;
+  // private float _referenceFrequency = 0.0f;
+
+  // void Start() {
+  //   _playerOriginY = transform.position.y;
+  //   _referenceFrequency = Musician.ReferenceFrequency;
+  // }
+
   void Update() {
-    if (GameManager.Instance.State == GameState.RUNNING && Input.GetMouseButtonDown(0)) {
-      OnClick();
+    if (GameManager.Instance.State != GameState.RUNNING) {
+      return;
     }
 
-    // TODO: pitch shift could be cool
-    // Musician.ReferenceFrequency =
-    //     _referenceFrequency * (1.0f + _playerOriginY - transform.position.y);
+    // Movement noise.
+    _instrument.Source.volume = character.velocity.sqrMagnitude;
+
+    // Fork interaction.
+    bool hasAutomatonOnTarget = false;
+    Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+    if (Physics.Raycast(ray, out var hitInfo, maxInteractionDistance)) {
+      hasAutomatonOnTarget = (hitInfo.collider.tag == "Automaton");
+    }
+
+    bool isHeld = Input.GetMouseButton(0);
+    Vector3 scale = (isHeld ? clickScale : (hasAutomatonOnTarget ? hoverScale : 1.0f)) * _idleScale;
+
+    if (Input.GetMouseButtonDown(0) && hasAutomatonOnTarget) {
+      // fork.localScale = scale;
+      OnClick(hitInfo.transform.parent.GetComponent<Automaton>());
+    }
 
     fork.localRotation =
         Quaternion.AngleAxis((float)GameManager.Instance.Position * 90.0f, Vector3.one);
-    fork.localScale =
-        Vector3.Lerp(fork.localScale, idleScale * Vector3.one, Time.deltaTime * clickScaleSpeed);
+    fork.localScale = Vector3.Lerp(fork.localScale, scale, Time.deltaTime * clickScaleSpeed);
   }
 
-  void OnClick() {
-    fork.localScale = clickScale * Vector3.one;
-
+  void OnClick(Automaton automaton) {
     // Toggle automaton.
-    Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
-    if (Physics.Raycast(ray, out var hitInfo)) {
-      if (hitInfo.collider.tag == "Automaton") {
-        var automaton = hitInfo.transform.parent.GetComponent<Automaton>();
-        if (automaton != null) {
-          automaton.Toggle();
-        }
-      }
+    if (automaton != null) {
+      automaton.Toggle();
     }
   }
 }
