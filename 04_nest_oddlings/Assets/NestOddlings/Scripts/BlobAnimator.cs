7a2f48db118709aabac974fec0043f8f25eaf8d0
alper gungormusler <anokta@gmail.com>
2025-05-07 22:01:14 -0400

    add poc instrument

--- 04_nest_oddlings/Assets/NestOddlings/Scripts/BlobAnimator.cs
+++ 04_nest_oddlings/Assets/NestOddlings/Scripts/BlobAnimator.cs
@@ -16,38 +16,55 @@ public enum BlobAnimState {
 
 public class BlobAnimator : MonoBehaviour {
   public Animator anim;
+  public Instrument instrument;
   public Metronome metronome;
 
   public float playbackMultiplier = 0.5f;
+  public float animCrossfadeTime = 0.1f;
 
-  public BlobAnimState initState;
-  public BlobAnimState barState;
+  public BlobAnimState idleState;
+  public BlobAnimState noteOnState;
 
   private string _currentState = null;
+  private string _targetState = null;
 
   private void OnEnable() {
     metronome.OnBeat += OnBeat;
-    _currentState = GetAnimStateString(initState);
+    instrument.OnNoteOn += OnNoteOn;
+    instrument.OnNoteOff += OnNoteOff;
+    _currentState = GetAnimStateString(idleState);
+    _targetState = _currentState;
   }
 
   private void OnDisable() {
     metronome.OnBeat -= OnBeat;
+    instrument.OnNoteOn -= OnNoteOn;
+    instrument.OnNoteOff -= OnNoteOff;
   }
 
   private void Update() {
     float playbackTime = Mathf.Repeat(playbackMultiplier * (float)metronome.Position, 1.0f);
-    anim.Play(_currentState, 0, playbackTime);
-  }
-
-  private void OnBeat(int bar, int beat) {
-    if (beat == 0) {
-      _currentState = GetAnimStateString(barState);
+    if (_currentState != _targetState) {
+      anim.CrossFade(_targetState, animCrossfadeTime, 0, playbackTime);
     } else {
-      _currentState = GetAnimStateString(initState);
+      anim.Play(_currentState, 0, playbackTime);
     }
   }
 
   private string GetAnimStateString(BlobAnimState state) {
     return "CharacterArmature|" + state.ToString();
+  }
+
+  private void OnBeat(int bar, int beat) {
+    _currentState = _targetState;
+  }
+
+  private void OnNoteOn(float pitch) {
+    _targetState = GetAnimStateString(noteOnState);
+    // _currentState = _targetState;  // immediately transition for better responsivity.
+  }
+
+  private void OnNoteOff(float pitch) {
+    _targetState = GetAnimStateString(idleState);
   }
 }
